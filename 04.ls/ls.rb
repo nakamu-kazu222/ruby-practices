0ba9cded1ebc91f1c4eba2de51e98e0ea6061a39
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-04-10 16:57:48 +0900

    lsコマンドプログラム修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,51 +3,59 @@
 # 行数指定
 COLUMN_COUNT = 3
 
-# 全ファイル名の文字数を揃える
-def chara_length
-  ary = Dir.glob('*')
-  max_chara = ary.max_by(&:size)
-  ary = ary.map { |max_chara_space| max_chara_space.ljust(max_chara.size + 6) }
-  file_set(ary)
+# ファイル名の文字数を揃える
+def current_directory_get_file
+  Dir.glob('*')
+end
+
+# ファイル名の文字数を揃える
+def align_file_characters(array_files)
+  max_characters = array_files.max_by(&:size)
+  array_files = array_files.map { |max_characters_space| max_characters_space.ljust(max_characters.size + 6) }
 end
 
 # ファイル名を名前順に縦に並べる
-def file_set(ary)
-  column_number = ary.size.quo(COLUMN_COUNT).ceil
-  column_file = []
-  ary.each_slice(column_number) { |file| column_file << file }
+def sort_file_vertical(array_files)
+  column_number = array_files.size.quo(COLUMN_COUNT).ceil
+  column_files = []
+  array_files.each_slice(column_number) { |file| column_files << file }
   print_ls = []
-  COLUMN_COUNT.times do |i|
+  column_files.count.times do |i|
     if i.zero?
-      column_file[i].zip(column_file[i + 1]) do |ls_file1|
-        print_ls << ls_file1
+      if column_files[i + 1].nil?
+        print_ls << column_files[i]
+        break
+      else
+        column_files[i].zip(column_files[i + 1]) do |ls_file1|
+          print_ls << ls_file1
+        end
       end
 
-    elsif i < (COLUMN_COUNT - 1)
+    elsif i < (column_files.count - 1)
       x = 0
       y = i
-      column_number.times do |column_file_count|
-        next unless column_file_count < column_number
+      column_number.times do |column_files_count|
+        next unless column_files_count < column_number
 
-        print_ls[x] << column_file[y + 1][x]
+        print_ls[x] << column_files[y + 1][x]
         x += 1
       end
-
-    else
-      break
     end
     i + 1
   end
-  display(print_ls)
+  array_files = print_ls
 end
 
-def display(print_ls)
-  print_ls.each do |secondary_array|
-    secondary_array.each do |primary_array|
-      print primary_array
+def display_ls(print_ls)
+  print_ls.each do |secondary_array_files|
+    secondary_array_files.each do |primary_array_files|
+      print primary_array_files
     end
     print "\n"
   end
 end
 
-chara_length
+array_files = current_directory_get_file
+array_files = align_file_characters(array_files)
+array_files = sort_file_vertical(array_files)
+display_ls(array_files)
