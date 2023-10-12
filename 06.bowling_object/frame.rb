2309f6c438701fee69cb004c34b843d8799d7b7f
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-10-12 17:45:09 +0900

    Frameクラスを作成した
    1フレーム内の1つ目の投球と2つ目の投球を取得し、スコアを計算する
    ストライクとスペアの条件を決めている

---
+++ 06.bowling_object/frame.rb
@@ -0,0 +1,20 @@
+class Frame
+  attr_accessor :first_shot, :second_shot
+
+  def initialize(first_shot, second_shot)
+    @first_shot = first_shot
+    @second_shot = second_shot
+  end
+
+  def score
+    @first_shot + @second_shot
+  end
+
+  def strike?
+    @first_shot == 10
+  end
+
+  def spare?
+    !strike? && score == 10
+  end
+end
